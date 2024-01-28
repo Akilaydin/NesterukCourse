7cba7abd7731794b9d948257776741ef064e8e3b
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-01-28 15:49:53 +0300

    [SOLID.2] [LSP] LSP examples

---
+++ SOLIDPrinciples.2/LSP/LSPClasses.cs
@@ -0,0 +1,40 @@
+﻿namespace SOLIDPrinciples.LSP;
+
+public class Rectangle
+{
+	public int Width { get; set; }
+	public int Height { get; set; }
+
+	public Rectangle() {}
+
+	public Rectangle(int width, int height)
+	{
+		Width = width;
+		Height = height;
+	}
+
+	public bool IsSquare => Width == Height;
+
+	public int Area {
+		get {
+			Console.WriteLine("Area");
+			return Width * Height;
+		}
+	}
+
+	public override string ToString()
+	{
+		return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+	}
+}
+
+public class Square : Rectangle
+{
+	public new int Height { set => base.Height = base.Width = value; }
+	public new int Width  { set => base.Height = base.Width = value; }
+
+	public Square(int side)
+	{
+		Width = Height = side;
+	}
+}
--- SOLIDPrinciples.2/Program.cs
+++ SOLIDPrinciples.2/Program.cs
@@ -1,18 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using SOLIDPrinciples.LSP;
+
 using static System.Console;
 
-var apple = new Product(name: "Apple", color: Color.Green, size: Size.Small);
-var tree = new Product(name: "Tree", color: Color.Green, size: Size.Large);
-var house = new Product(name: "House", color: Color.Blue, size: Size.Large);
+var rc = new Rectangle(2,3);
+PrintWidth(rc);
 
-Product[] products = { apple, tree, house };
+var sq = new Square(5);
+PrintWidth(sq);
 
-var betterFilter = new GoodProductFilter();
-
-var t = new ColorSpecification { TargetColor = Color.Green } & new SizeSpecification { TargetSize = Size.Large };
-
-foreach (var product in betterFilter.Filter(products, t))
+static void PrintWidth(Rectangle r)
 {
-	WriteLine(product);
+	int width = r.Width;
+	r.Height = 10;
+	WriteLine($"Expected area of {10 * width}, got {r.Area}");
 }
