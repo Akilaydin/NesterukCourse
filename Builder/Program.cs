a81b28931f7361a91d684408ee18e2cf97c031df
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-01-31 20:06:50 +0300

    [Builder.3] [Functional builder] Builder examples

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,61 +1,53 @@
 ﻿#region
-using System.Globalization;
-using System.Reflection;
-using System.Reflection.Emit;
-
 using static System.Console;
 #endregion
 
 // Person person = new PersonBuilder().Lives.At("StreetName").In("Moscow").WithPostcode("122220").Works.AsA("Developer").At("Microsoft").Earns(5000);
 // WriteLine(person);
-var me = Person.New
-	.Called("Artem")
-	.WorksAsA("Unity developer")
-	.Build();
 
-WriteLine(me);
+var builder = new PersonBuilder();
+var person = builder.Called("Artem").WorksAsA("Unity developer").Build();
+WriteLine(person.Name);
 
 public class Person
 {
-	public static Builder New => new Builder();
+	public string Name, Position;
+}
+
+public sealed class PersonBuilder : FunctionalBuilder<Person, PersonBuilder>;
+
+public static class PersonBuilderExtensions
+{
+	public static PersonBuilder Called(this PersonBuilder builder, string name)
+	{
+		builder.AddAction(person => person.Name = name);
+
+		return builder;
+	}
+	public static PersonBuilder WorksAsA(this PersonBuilder builder, string position)
+	{
+		builder.AddAction(person => person.Position = position);
+
+		return builder;
+	}
+}
+
+public abstract class FunctionalBuilder<TSubject, TSelf> 
+	where TSelf : FunctionalBuilder<TSubject, TSelf>
+	where TSubject : new()
+{
+	private readonly List<Func<TSubject, TSubject>> _buildActions = new();
 	
-	public string Name;
-
-	public string Position;
-
-	public class Builder : PersonJobBuilder<Builder>
+	public TSubject Build() => _buildActions.Aggregate(new TSubject(), (subject, buildAction) => buildAction(subject));
+	
+	public TSelf AddAction(Action<TSubject> action)
 	{
-		internal Builder() {}
-	}
-}
+		_buildActions.Add(subject =>
+		{
+			action(subject);
+			return subject;
+		});
 
-public abstract class PersonBuilder
-{
-	protected Person Person = new Person();
-
-	public Person Build()
-	{
-		return Person;
-	}
-}
-
-public class PersonInfoBuilder<TSelf> : PersonBuilder
-	where TSelf : PersonInfoBuilder<TSelf>
-{
-	public TSelf Called(string name)
-	{
-		Person.Name = name;
-		return (TSelf) this;
-	}
-}
-
-public class PersonJobBuilder<TSelf> 
-	: PersonInfoBuilder<TSelf>
-	where TSelf : PersonJobBuilder<TSelf>
-{
-	public TSelf WorksAsA(string position)
-	{
-		Person.Position = position;
-		return (TSelf) this;
+		return (TSelf)this;
 	}
 }
