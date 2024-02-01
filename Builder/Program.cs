6d8d3112553aa50e23d1184cfd3ed711a6e54433
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-02-01 19:07:01 +0300

    [Builder.3] [Parametricla builder] Builder examples

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -5,49 +5,68 @@ using static System.Console;
 // Person person = new PersonBuilder().Lives.At("StreetName").In("Moscow").WithPostcode("122220").Works.AsA("Developer").At("Microsoft").Earns(5000);
 // WriteLine(person);
 
-var builder = new PersonBuilder();
-var person = builder.Called("Artem").WorksAsA("Unity developer").Build();
-WriteLine(person.Name);
+var service = new EmailService();
+service.SendEmail(builder => builder.To("Artem").AddSubject("Subject").AddBody("Body").From("A mystery friend"));
 
-public class Person
+public class EmailService
 {
-	public string Name, Position;
-}
-
-public sealed class PersonBuilder : FunctionalBuilder<Person, PersonBuilder>;
-
-public static class PersonBuilderExtensions
-{
-	public static PersonBuilder Called(this PersonBuilder builder, string name)
+	public class EmailBuilder
 	{
-		builder.AddAction(person => person.Name = name);
-
-		return builder;
-	}
-	public static PersonBuilder WorksAsA(this PersonBuilder builder, string position)
-	{
-		builder.AddAction(person => person.Position = position);
-
-		return builder;
-	}
-}
-
-public abstract class FunctionalBuilder<TSubject, TSelf> 
-	where TSelf : FunctionalBuilder<TSubject, TSelf>
-	where TSubject : new()
-{
-	private readonly List<Func<TSubject, TSubject>> _buildActions = new();
-	
-	public TSubject Build() => _buildActions.Aggregate(new TSubject(), (subject, buildAction) => buildAction(subject));
-	
-	public TSelf AddAction(Action<TSubject> action)
-	{
-		_buildActions.Add(subject =>
+		private readonly Email _email;
+		internal EmailBuilder(Email email)
 		{
-			action(subject);
-			return subject;
-		});
+			_email = email;
+		}
 
-		return (TSelf)this;
+		public EmailBuilder From(string from)
+		{
+			_email.From = from;
+			
+			return this;
+		}
+		
+		public EmailBuilder AddBody(string body)
+		{
+			_email.Body = body;
+			
+			return this;
+		}
+		
+		public EmailBuilder AddSubject(string subject)
+		{
+			_email.Subject = subject;
+			
+			return this;
+		}
+		
+		public EmailBuilder To(string to)
+		{
+			_email.To = to;
+			
+			return this;
+		}
+	}
+	
+	internal class Email
+	{
+		public string From, To, Subject, Body;
+
+		public override string ToString()
+		{
+			return $"{From} {To} {Subject} {Body}";
+		}
+	}
+	
+	public void SendEmail(Action<EmailBuilder> buildEmailAction)
+	{
+		var resultEmail = new Email();
+		buildEmailAction?.Invoke(new EmailBuilder(resultEmail));
+		SendEmailInternal(resultEmail);
+	}
+
+	private void SendEmailInternal(Email email)
+	{
+		WriteLine("Sending email internal impl " + email);
 	}
 }
+
