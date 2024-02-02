b9e80d212a5011fb5620095d3c6144db3d0e3bad
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-02-02 11:59:45 +0300

    [Builder.3] [Exercise] Exercise was made

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,72 +1,55 @@
 ﻿#region
+using System.Text;
+
 using static System.Console;
 #endregion
 
-// Person person = new PersonBuilder().Lives.At("StreetName").In("Moscow").WithPostcode("122220").Works.AsA("Developer").At("Microsoft").Earns(5000);
-// WriteLine(person);
+Write(new ClassBuilder("Person")
+	.AddField("Name", "string", "public")
+	.AddField("Age", "int", "private")
+	.AddField("Position", "string", "private")
+	.Build());
 
-var service = new EmailService();
-service.SendEmail(builder => builder.To("Artem").AddSubject("Subject").AddBody("Body").From("A mystery friend"));
-
-public class EmailService
+public class ClassBuilder(string className)
 {
-	public class EmailBuilder
-	{
-		private readonly Email _email;
-		internal EmailBuilder(Email email)
-		{
-			_email = email;
-		}
-
-		public EmailBuilder From(string from)
-		{
-			_email.From = from;
-			
-			return this;
-		}
-		
-		public EmailBuilder AddBody(string body)
-		{
-			_email.Body = body;
-			
-			return this;
-		}
-		
-		public EmailBuilder AddSubject(string subject)
-		{
-			_email.Subject = subject;
-			
-			return this;
-		}
-		
-		public EmailBuilder To(string to)
-		{
-			_email.To = to;
-			
-			return this;
-		}
-	}
+	private List<Field> _fields = [];
 	
-	internal class Email
+	public Class Build()
 	{
-		public string From, To, Subject, Body;
-
-		public override string ToString()
-		{
-			return $"{From} {To} {Subject} {Body}";
-		}
-	}
-	
-	public void SendEmail(Action<EmailBuilder> buildEmailAction)
-	{
-		var resultEmail = new Email();
-		buildEmailAction?.Invoke(new EmailBuilder(resultEmail));
-		SendEmailInternal(resultEmail);
+		return new Class(className, _fields);
 	}
 
-	private void SendEmailInternal(Email email)
+	public ClassBuilder AddField(string fieldName, string fieldType, string accessModifier)
 	{
-		WriteLine("Sending email internal impl " + email);
+		_fields.Add(new Field(fieldName,fieldType, accessModifier));
+		
+		return this;
 	}
 }
 
+public record Class(string ClassName, List<Field> Fields)
+{
+	public override string ToString()
+	{
+		var stringBuilder = new StringBuilder();
+
+		stringBuilder.AppendLine($"public class {ClassName}");
+		stringBuilder.AppendLine("{");
+
+		foreach (var field in Fields)
+		{
+			stringBuilder.AppendLine($"    {field.ToString()}");
+		}
+
+		stringBuilder.AppendLine("}");
+
+		return stringBuilder.ToString();
+	}
+}
+public record Field(string FieldName, string FieldType, string AccessModifier)
+{
+	public override string ToString()
+	{
+		return $"{AccessModifier} {FieldType} {FieldName}";
+	}
+}
