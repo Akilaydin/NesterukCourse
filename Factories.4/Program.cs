c99f6577b0c431e5de5648490f28ab3002801437
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-02-03 15:46:41 +0300

    [Factories.4] [Exercise] Exercise was made

--- Factories.4/Program.cs
+++ Factories.4/Program.cs
@@ -1,34 +1,14 @@
 ﻿#region
 #endregion
 
-Point.PointsFactory.NewCartesianPoint(3, 2);
-
-
-public class Point
+public class PersonFactory
 {
-	private double _x, _y;
-
-	private Point(double y, double x)
-	{
-		_y = y;
-		_x = x;
-	}
+	private static int s_id;
 	
-	public override string ToString()
+	public Person CreatePerson(string name)
 	{
-		return $"{nameof(_x)}: {_x}, {nameof(_y)}: {_y}";
-	}
-	
-	public class PointsFactory //Factory
-	{
-		public static Point NewCartesianPoint(double x, double y) //Factory method
-		{
-			return new Point(x, y);
-		}
-
-		public static Point NewPolarPoint(double rho, double theta) //Factory method
-		{
-			return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-		}
+		s_id++;
+		return new Person(s_id, name);
 	}
 }
+public record Person (int Id, string Name);
