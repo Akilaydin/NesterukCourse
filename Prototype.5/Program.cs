c2523f65c841e9f8b616685fd63d40af7d18d1e4
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-02-03 20:31:29 +0300

    [Prototype.5] [Copying constructor] Examples with copying constructor

---
+++ Prototype.5/Program.cs
@@ -0,0 +1,58 @@
+﻿using static System.Console;
+
+var john = new Person("John", new Address("123 London Road", "London", "UK"));
+
+var jane = new Person(john) 
+{
+	Name = "Jane"
+};
+
+WriteLine(john);
+WriteLine(jane);
+
+public class Address
+{
+	public string StreetAddress, City, Country;
+
+	public Address(string streetAddress, string city, string country)
+	{
+		StreetAddress = streetAddress;
+		City = city;
+		Country = country;
+	}
+
+	public Address(Address other) //Copying constructor
+	{
+		StreetAddress = other.StreetAddress;
+		City = other.City;
+		Country = other.Country;
+	}
+
+	public override string ToString()
+	{
+		return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(City)}: {City}, {nameof(Country)}: {Country}";
+	}
+}
+
+public class Person
+{
+	public string Name;
+	public Address Address;
+
+	public Person(string name, Address address)
+	{
+		Name = name;
+		Address = address;
+	}
+
+	public Person(Person other) //Copying constructor
+	{
+		Name = other.Name;
+		Address = new Address(other.Address);
+	}
+
+	public override string ToString()
+	{
+		return $"{nameof(Name)}: {Name}, {nameof(Address)}: {Address}";
+	}
+}
