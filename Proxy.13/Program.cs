d03ff7a1b4211315355c76b1c13beddaace9fa08
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-02-14 17:07:31 +0300

    [Proxy.13] Proxy examples

---
+++ Proxy.13/Program.cs
@@ -0,0 +1,36 @@
+﻿ICar car = new CarProxy(new Car(), new Driver { Age = 15 });
+
+car.Drive();
+
+internal interface ICar
+{
+	void Drive();
+}
+
+class Car : ICar
+{
+	public void Drive()
+	{
+		Console.WriteLine("Car is driven");
+	}
+}
+
+class Driver
+{
+	public required int Age;
+}
+
+class CarProxy(Car car, Driver driver) : ICar
+{
+	public void Drive()
+	{
+		if (driver.Age <= 16)
+		{
+			Console.WriteLine("Can't drive");
+		}
+		else
+		{
+			car.Drive();
+		}
+	}
+}
