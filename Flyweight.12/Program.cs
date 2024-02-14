2fc2d7a057211535c8525f01727e170117377ab5
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-02-14 14:09:07 +0300

    [Flyweight.12] Flyweight examples

---
+++ Flyweight.12/Program.cs
@@ -0,0 +1,34 @@
+﻿var user = new User("Artem Ovchinnikov");
+var user2 = new User("Eugeni Ovchinnikov");
+var user3 = new User("user Ovchinnikov");
+var user4 = new User("user2 Ovchinnikov");
+
+Console.WriteLine(User.s_strings.Count); //outputs 3 because Ovchinnikov didn't allocate twice
+
+public class User
+{
+	public string FullName => string.Join(" ", _names.Select(index => s_strings[index])); //cache
+	
+	public static List<string> s_strings = new();
+	
+	private int[] _names;
+
+	public User(string fullName)
+	{
+		_names = fullName.Split(' ').Select(GetOrAdd).ToArray();
+	}
+
+	private int GetOrAdd(string s)
+	{
+		var index = s_strings.IndexOf(s);
+		
+		if (index != -1)
+		{
+			return index;
+		}
+		
+		s_strings.Add(s);
+		
+		return s_strings.Count - 1;
+	}
+}
