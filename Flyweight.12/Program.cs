7b226ab0c03366a60cb6a773780f5dc82ba29f72
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-02-14 14:38:25 +0300

    [Flyweight.12] Fix flyweight examples

--- Flyweight.12/Program.cs
+++ Flyweight.12/Program.cs
@@ -9,13 +9,12 @@ Console.WriteLine(text);
 public class FormattedText(string plainText)
 {
 	private readonly List<TextRange> _formatting = [];
+	private static readonly TextRangePool RangePool = new();
 
 	public TextRange GetRange(int start, int end)
 	{
-		var range = new TextRange {Start = start, End = end};
-		
+		var range = RangePool.GetTextRange(start, end, false, false, false);
 		_formatting.Add(range);
-		
 		return range;
 	}
 
@@ -25,30 +24,62 @@ public class FormattedText(string plainText)
 
 		for (int i = 0; i < plainText.Length; i++)
 		{
-			var character = plainText[i];
-			
+			var charAdded = false;
 			foreach (var format in _formatting)
 			{
-				if (format.Covers(i) && format.Capitalize)
+				if (format.Covers(i))
 				{
-					character = char.ToUpperInvariant(character);
+					var character = format.Capitalize ? char.ToUpperInvariant(plainText[i]) : plainText[i];
+					sb.Append(character);
+					charAdded = true;
+					break;
 				}
-
-				sb.Append(character);
+			}
+			if (!charAdded)
+			{
+				sb.Append(plainText[i]);
 			}
 		}
-		
+
 		return sb.ToString();
 	}
+}
 
-	public class TextRange
+
+public class TextRange
+{
+	public required int Start, End;
+	public required bool Capitalize, Bold, Italic;
+
+	public bool Covers(int position)
 	{
-		public required int Start, End;
-		public bool Capitalize, Bold, Italic;
+		return position >= Start && position <= End;
+	}
+}
 
-		public bool Covers(int position)
+public class TextRangePool
+{
+	private readonly List<TextRange> _pool = [];
+
+	public TextRange GetTextRange(int start, int end, bool capitalize, bool bold, bool italic)
+	{
+		var existingRange = _pool.FirstOrDefault(tr => tr.Start == start && tr.End == end 
+			&& tr.Capitalize == capitalize && tr.Bold == bold 
+			&& tr.Italic == italic);
+		if (existingRange != null)
 		{
-			return position >= Start && position <= End;
+			return existingRange;
 		}
+
+		var newTextRange = new TextRange
+		{
+			Start = start, 
+			End = end, 
+			Capitalize = capitalize, 
+			Bold = bold, 
+			Italic = italic
+		};
+		_pool.Add(newTextRange);
+		return newTextRange;
 	}
 }
