f8d1938d42cdab08f11e9df16204cd601f615a1b
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-02-15 23:17:47 +0300

    [ChainOfResponsibility.14] More complex modifiers examples

--- ChainOfResponsibility.14/Program.cs
+++ ChainOfResponsibility.14/Program.cs
@@ -1,96 +1,116 @@
-﻿var creature = new Creature("Goblin", 2, 4);
+﻿var game = new Game();
+var creature = new Creature(game, "Goblin", 2, 4);
+Console.WriteLine(creature.ToString());
 
-var root = new CreatureModifier(creature);
-root.AddModifier(new DoubleAttackModifier(creature));
-root.AddModifier(new DoubleAttackModifier(creature));
-root.AddModifier(new DoubleAttackModifier(creature));
-root.AddModifier(new RemoveAllModifiersModifier(creature));
-root.AddModifier(new DoubleAttackModifier(creature));
-root.AddModifier(new DoubleAttackModifier(creature));
-root.AddModifier(new DoubleAttackModifier(creature));
 
-root.AddModifier(new DoubleDefenseModifier(creature));
-root.AddModifier(new DoubleDefenseModifier(creature));
-root.AddModifier(new DoubleDefenseModifier(creature));
-root.AddModifier(new DoubleDefenseModifier(creature));
-root.AddModifier(new DoubleDefenseModifier(creature));
-
-root.Handle();
+using (new DoubleAttackModifier(game, creature))
+{
+	Console.WriteLine(creature.ToString());
+	
+	using (new DoubleAttackModifier(game, creature))
+	{
+		Console.WriteLine(creature.ToString());
+		
+		using (new DoubleDefenseModifier(game, creature))
+		{
+			Console.WriteLine(creature.ToString());
+		}
+	}
+}
 
 Console.WriteLine(creature.ToString());
 
-public class Creature(string name, int attack, int defense)
+public class Query(string creatureName, QueryArgument argument, int value)
+{
+	public string CreatureName = creatureName;
+	public QueryArgument Argument = argument;
+	public int Value = value;
+}
+
+public enum QueryArgument
+{
+	Attack, Defense
+}
+
+public class Game
+{
+	public event EventHandler<Query>? Queries;
+
+	public void PerformQuery(object sender, Query q)
+	{
+		Queries?.Invoke(sender, q);
+	}
+}
+
+public class Creature(Game game, string name, int attack, int defense)
 {
 	public string Name = name;
-	public int Attack = attack, Defense = defense;
+	
+	public int Attack {
+		get {
+			var query = new Query(Name, QueryArgument.Attack, attack);
+			game.PerformQuery(this, query);
+			return query.Value;
+		}
+	}
 
+	public int Defense {
+		get {
+			var query = new Query(Name, QueryArgument.Defense, defense);
+			game.PerformQuery(this, query);
+			return query.Value;
+		}
+	}
+	
 	public override string ToString()
 	{
 		return $"{nameof(Name)}: {Name}, {nameof(Attack)}: {Attack}, {nameof(Defense)}: {Defense}";
 	}
 }
 
-public class CreatureModifier(Creature creature)
+public abstract class CreatureModifier : IDisposable
 {
-	protected readonly Creature Creature = creature;
-	private CreatureModifier? _next;
+	private readonly Game _game;
+	private readonly Creature _creature;
 	
-	public virtual void Handle() => _next?.Handle();
-
-	public void AddModifier(CreatureModifier cm)
+	protected CreatureModifier(Game game, Creature creature)
 	{
-		if (_next != null)
+		_game = game;
+		_creature = creature;
+		
+		game.Queries += HandleQuery;
+	}
+
+	public void Dispose()
+	{
+		_game.Queries -= HandleQuery;
+	}
+
+	public abstract void HandleQuery(object? sender, Query query);
+}
+
+public class DoubleAttackModifier(Game game, Creature creature) : CreatureModifier(game, creature)
+{
+	private readonly Creature _creature = creature;
+
+	public override void HandleQuery(object? sender, Query query)
+	{
+		if (query.CreatureName == _creature.Name && query.Argument == QueryArgument.Attack)
 		{
-			_next.AddModifier(cm);
+			query.Value *= 2;
 		}
-		else
+	}
+}
+
+public class DoubleDefenseModifier(Game game, Creature creature) : CreatureModifier(game, creature)
+{
+	private readonly Creature _creature = creature;
+	
+	public override void HandleQuery(object? sender, Query query)
+	{
+		if (query.CreatureName == _creature.Name && query.Argument == QueryArgument.Defense)
 		{
-			_next = cm;
+			query.Value *= 2;
 		}
-	}
-}
-
-public class RemoveAllModifiersModifier(Creature creature) : CreatureModifier(creature) 
-{
-	//note: absence of base.Handle() so we interrupt the chain
-	public override void Handle()
-	{
-		Console.WriteLine("No modifiers further on");
-	}
-}
-
-public class RemoveDefenseModifier(Creature creature) : CreatureModifier(creature) 
-{
-	public override void Handle()
-	{
-		Console.WriteLine("Removing the defense");
-		
-		Creature.Defense = 0;
-
-		base.Handle();
-	}
-}
-
-public class DoubleAttackModifier(Creature creature) : CreatureModifier(creature) 
-{
-	public override void Handle()
-	{
-		Console.WriteLine("Doubling the attack");
-		
-		Creature.Attack *= 2;
-		
-		base.Handle();
-	}
-}
-
-public class DoubleDefenseModifier(Creature creature) : CreatureModifier(creature) 
-{
-	public override void Handle()
-	{
-		Console.WriteLine("Doubling the defense");
-		
-		Creature.Defense *= 2;
-		
-		base.Handle();
 	}
 }
