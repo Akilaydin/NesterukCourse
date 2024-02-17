c4a9706414e05f6f317e216b254a04f32ab4d954
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-02-17 21:10:59 +0300

    [Interpreter.16] More interpreter examples

--- Interpreter.16/Program.cs
+++ Interpreter.16/Program.cs
@@ -1,9 +1,70 @@
-﻿using System.Text;
+﻿#region
+using System.Text;
+#endregion
 
 var expression = "(13+4)-(12+1)";
 var tokens = Lex(expression);
 Console.WriteLine(string.Join("\t", tokens));
-Console.WriteLine(tokens.Count);
+
+var parsed = Parse(tokens);
+Console.WriteLine($"{expression} = {parsed.Value}");
+static IElement Parse(IReadOnlyList<Token> tokens)
+{
+	var result = new BinaryOperation();
+	var haveLhs = false;
+	for (var i = 0; i < tokens.Count; i++)
+	{
+		var token = tokens[i];
+
+		switch (token.TokenType)
+		{
+			case Token.Type.Integer:
+				var integer = new Integer(int.Parse(token.Text));
+				if (!haveLhs)
+				{
+					result.Left = integer;
+					haveLhs = true;
+				}
+				else
+				{
+					result.Right = integer;
+				}
+				break;
+			case Token.Type.Plus:
+				result.OperationType = OperationType.Addition;
+				break;
+			case Token.Type.Minus:
+				result.OperationType = OperationType.Subtraction;
+				break;
+			case Token.Type.Lparen:
+				var j = i;
+				for (; j < tokens.Count; ++j)
+				{
+					if (tokens[j].TokenType == Token.Type.Rparen)
+					{
+						break;
+					}
+				}
+
+				var subexpression = tokens.Skip(i + 1).Take(j - i - 1).ToList();
+				var element = Parse(subexpression);
+				if (!haveLhs)
+				{
+					result.Left = element;
+					haveLhs = true;
+				}
+				else
+				{
+					result.Right = element;
+				}
+				i = j;
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+	}
+	return result;
+}
 
 List<Token> Lex(string input)
 {
@@ -30,7 +91,7 @@ List<Token> Lex(string input)
 		else
 		{
 			var sb = new StringBuilder(input[i].ToString());
-			for (int j = i + 1; j < input.Length; ++j)
+			for (var j = i + 1; j < input.Length; ++j)
 			{
 				if (char.IsDigit(input[j]))
 				{
@@ -45,8 +106,41 @@ List<Token> Lex(string input)
 			}
 		}
 	}
-	
+
 	return result;
+}
+
+public interface IElement
+{
+	int Value { get; }
+}
+
+public class Integer(int value) : IElement
+{
+	public int Value => value;
+}
+
+public enum OperationType
+{
+	Addition, Subtraction
+}
+
+public class BinaryOperation : IElement
+{
+	public OperationType OperationType;
+	public IElement Left;
+	public IElement Right;
+
+	public int Value => CalculateValue();
+
+	private int CalculateValue()
+	{
+		return OperationType switch {
+			OperationType.Addition => Left.Value + Right.Value,
+			OperationType.Subtraction => Left.Value - Right.Value,
+			_ => throw new ArgumentException()
+		};
+	}
 }
 
 public class Token
@@ -56,12 +150,12 @@ public class Token
 		Integer, Plus, Minus, Lparen, Rparen
 	}
 
-	public Type MyType;
+	public Type TokenType;
 	public string Text;
 
 	public Token(Type type, string text)
 	{
-		MyType = type;
+		TokenType = type;
 		Text = text;
 	}
 
