c70fd4c17d07b5d5fb6c5f07b32290927863c108
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-02-20 12:53:31 +0300

    [Observer.21] Observer examples

---
+++ Observer.21/Program.cs
@@ -0,0 +1,30 @@
+﻿var person = new Person();
+
+void OnPersonOnFallsIll(object? sender, FallsIllEventArgs eventArgs)
+{
+	Console.WriteLine($"Calling a doctor to {eventArgs.Address}");
+}
+
+person.FallsIll += OnPersonOnFallsIll;
+person.FallsIll += OnPersonOnFallsIll;
+
+person.FallIll();
+
+person.FallsIll -= OnPersonOnFallsIll;
+
+person.FallIll();
+
+public class FallsIllEventArgs : EventArgs
+{
+	public required string Address;
+}
+
+public class Person
+{
+	public event EventHandler<FallsIllEventArgs> FallsIll;
+
+	public void FallIll()
+	{
+		FallsIll?.Invoke(this, new FallsIllEventArgs {Address = "Moscow"});
+	}
+}
