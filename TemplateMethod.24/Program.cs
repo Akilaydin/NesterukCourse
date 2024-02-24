b70a665ba8d7766e0b4a94b4607a2e76b5860feb
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-02-24 22:38:25 +0300

    [TemplateMethod.24] Template Method example

---
+++ TemplateMethod.24/Program.cs
@@ -0,0 +1,48 @@
+﻿using static System.Console;
+
+Game game = new Chess();
+game.Run();
+
+public abstract class Game(int numberOfPlayers)
+{
+	public void Run()
+	{
+		Start();
+		while (!HaveWinner)
+		{
+			TakeTurn();
+		}
+		
+		WriteLine($"Player {WinningPlayer} wins.");
+	}
+
+	protected abstract void Start();
+	protected abstract bool HaveWinner { get; }
+	protected abstract void TakeTurn();
+	protected abstract int WinningPlayer { get; }
+
+	protected int CurrentPlayer;
+
+	protected readonly int NumberOfPlayers = numberOfPlayers;
+}
+
+public class Chess() : Game(2)
+{
+	protected override void Start()
+	{
+		WriteLine($"Starting a game of chess with {NumberOfPlayers} players.");
+	}
+
+	protected override bool HaveWinner => _turn == s_maxTurns;
+
+	protected override void TakeTurn()
+	{
+		WriteLine($"Turn {_turn++} taken by player {CurrentPlayer}.");
+		CurrentPlayer = (CurrentPlayer + 1) % NumberOfPlayers;
+	}
+
+	protected override int WinningPlayer => CurrentPlayer;
+
+	private const int s_maxTurns = 10;
+	private int _turn = 1;
+}
