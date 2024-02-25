914f1ed0126d3267e8ed82ce676e5c24d96d6bd9
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-02-25 14:00:37 +0300

    [Visitor.25] Reflective visitor examples

--- Visitor.25/Program.cs
+++ Visitor.25/Program.cs
@@ -2,32 +2,50 @@
 
 var e = new AdditionExpression(new DoubleExpression(1), new AdditionExpression(new DoubleExpression(2), new DoubleExpression(3))); // 1 + (2 + 3)
 
-var sb = new StringBuilder();
-e.Print(sb);
+ExpressionPrinter.AppendExpression(e);
+ExpressionPrinter.Print();
 
-Console.WriteLine(sb.ToString());
+
+public class ExpressionPrinter
+{
+	private static StringBuilder s_builder = new();
+
+	public static void Print()
+	{
+		Console.WriteLine(s_builder.ToString());
+	}
+	
+	public static void AppendExpression(Expression expression)
+	{
+		switch (expression)
+		{
+			case DoubleExpression e:
+				s_builder.Append(e.Value);
+				break;
+			case AdditionExpression e2:
+				s_builder.Append(value: '(');
+				AppendExpression(e2.Left);
+				s_builder.Append(value: '+');
+				AppendExpression(e2.Right);
+				s_builder.Append(value: ')');
+				break;
+		}
+	}
+
+	public static void Clear() => s_builder.Clear();
+}
 
 public abstract class Expression
 {
-	public abstract void Print(StringBuilder sb);
 }
 
 public class DoubleExpression(double value) : Expression
 {
-	public override void Print(StringBuilder sb)
-	{
-		sb.Append(value);
-	}
+	public readonly double Value = value;
 }
 
 public class AdditionExpression(Expression left, Expression right) : Expression
 {
-	public override void Print(StringBuilder sb)
-	{
-		sb.Append(value: '(');
-		left.Print(sb);
-		sb.Append(value: '+');
-		right.Print(sb);
-		sb.Append(value: ')');
-	}
+	public readonly Expression Right = right;
+	public readonly Expression Left = left;
 }
